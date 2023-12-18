47023baaadef506e25240f704fcdf73f9278e763
github.com <dhanukondasandhya8@gmail.com>
2023-12-18 17:25:20 +0530

    adding user

--- user.sh
+++ user.sh
@@ -87,7 +87,7 @@ systemctl start user &>> $LOGFILE
 
 VALIDATE $? "starting user"
 
-cp /home/centos/example.txt/mongo.repo /etc/yum.repos.d/mongo.repo $>> $LOGFILE
+cp /home/centos/example.txt/mongo.repo   /etc/yum.repos.d/mongo.repo $>> $LOGFILE
 
 VALIDATE $? "coping mongo repo"
 
